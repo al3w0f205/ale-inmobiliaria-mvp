16200b5a666f3325a3bc2ffe88614d2871d1b9f1
al3w0f205 <miguel.alejandro.figueroa@gmail.com>
2026-07-20 00:47:03 -0500

    fix: correct django initialization order in asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -9,17 +9,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             core.routing.websocket_urlpatterns
         )
     ),
 })
+
